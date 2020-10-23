844492332a64970c7f21a0b9f4e0c4d3f7eff626
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-10-23 13:51:19 +0900

    heroku

--- app.js
+++ app.js
@@ -69,4 +69,8 @@ app.post('/update/:id', (req, res) => {
       res.redirect('/');
     })
 });
-app.listen(3000);
+
+const port = process.env.PORT || 3000;
+    app.listen(port, () => { 
+        console.log("ポート" + port + "番で起動しました！")
+    })
